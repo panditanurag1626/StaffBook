33eaf7cba0fab7e573420890d26cd968ef06c6c8
unknown <panditanurag902606@gmail.com>
2026-07-09 13:00:29 +0530

    Add About page

---
+++ deploy.bat
@@ -0,0 +1,8 @@
+@echo off
+cd /d "D:\StaffBook_Final-master\StaffBook_Final-master"
+git add .
+git commit -m "Add About Us page"
+git push
+echo.
+echo Done! Vercel will deploy in ~2-3 minutes.
+pause
