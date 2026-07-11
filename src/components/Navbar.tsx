33c8a54796451ba1800b4753583c85c65c0c2f24
unknown <panditanurag902606@gmail.com>
2026-07-11 14:06:00 +0530

    second commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -148,7 +148,7 @@ const Navbar = () => {
           </div>
 
           {/* Center: Nav Items */}
-          <div className="flex-1 min-w-0 overflow-hidden">
+          <div className="flex-1 min-w-0">
             {user && !['/signin', '/signup'].includes(path) ? (
               <NavbarDesktop links={filteredLinks} currentPath={path} />
             ) : (
