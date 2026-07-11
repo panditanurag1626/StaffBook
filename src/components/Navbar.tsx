a0dd9edb4161c491cd691851c651cf492bb81d66
unknown <panditanurag902606@gmail.com>
2026-07-11 15:14:40 +0530

    second commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -144,7 +144,7 @@ const Navbar = () => {
           </div>
 
           {/* Center: Nav Items */}
-          <div className="flex-1 min-w-0 overflow-x-auto scrollbar-hide">
+          <div className="lg:flex-1 lg:min-w-0 overflow-x-auto scrollbar-hide">
             {user && !['/signin', '/signup'].includes(path) ? (
               <NavbarDesktop links={filteredLinks} currentPath={path} />
             ) : (
