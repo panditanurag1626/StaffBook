9aab0f8a44c04b78f13322ede7ed77fb26fb7228
unknown <panditanurag902606@gmail.com>
2026-07-11 15:55:50 +0530

    second commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,22 @@
 "use client";
-import HeroAndMakesDiff from "../components/Homepage/HeroAndMakesDiff";
-import FeatureCards from "../components/Homepage/FeatureCards";
-import JobSeekerJourney from "../components/Homepage/JobSeekerJourney";
-import EmployerJourney from "../components/Homepage/EmployerJourney";
-import CategoriesWithNews from "../components/Homepage/merge2and3";
+import HeroSection from "../components/Homepage/HeroSection";
+import CompanyLogoSection from "../components/Homepage/company-logo";
+import Categories from "../components/Homepage/section2";
+import ResumeSection from "../components/Homepage/resume";
+import PremiumSection from "../components/Homepage/PremiumSection";
+import CombinedChatJobSection from "../components/Homepage/liveAndchatSection";
+import ExpertsSection from "../components/Homepage/connection";
 
 export default function RootPage() {
   return (
-    <div className="bg-[#0D001A]">
-      <HeroAndMakesDiff />
-      <FeatureCards />
-      <JobSeekerJourney />
-      <EmployerJourney />
-      <CategoriesWithNews />
+    <div>
+      <HeroSection />
+      <CompanyLogoSection />
+      <Categories />
+      <ResumeSection />
+      <PremiumSection />
+      <CombinedChatJobSection />
+      <ExpertsSection />
     </div>
   );
 }
